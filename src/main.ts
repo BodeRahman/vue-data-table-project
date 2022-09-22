b634b9f5cff92ee1d24f7995846752e567eccb67
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2022-09-22 23:19:00 +0100

    Install bootstrap and font-awsome

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+/* Font Awesome CSS */
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
+
+/* Bootstrap CSS */
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
 import "./assets/main.css";
 
 const app = createApp(App);
