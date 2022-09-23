81cc2246a0e18fce39df995345e6ed2e582f3783
Abdulrahman Olagunju <abdulrahmanolagunju.1@gmail.com>
2022-09-23 08:29:51 +0100

    Installl revo-grid

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
 
 /* Bootstrap CSS */
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+
 
 import "./assets/main.css";
 
